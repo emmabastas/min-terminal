d58f7766bb7fd00b9e481a46295f53ad04c2e26e
Emma Bastås <emma.bastas@protonmail.com>
2024-11-29 23:18:51 +0100

    Parse ESC[2004h and ESC[2004l escape codes

--- termbuf.h
+++ termbuf.h
@@ -8,29 +8,30 @@
 
 #include "./CuTest.h"
 
-#define FLAG_LENGTH_0 0    // 0b00000000
-#define FLAG_LENGTH_1 1    // 0b00000001
-#define FLAG_LENGTH_2 2    // 0b00000010
-#define FLAG_LENGTH_3 3    // 0b00000011
-#define FLAG_LENGTH_4 4    // 0b00000100
-#define FLAG_LENGTH_MASK 7 // 0b00000111
-#define LAG_BOLD      8    // 0b00001000
-#define LAG_FAINT     16   // 0b00010000
-#define LAG_ITALIC    32   // 0b00100000
-#define LAG_UNDERLINE 64   // 0b01000000
-#define LAG_STRIKEOUT 128  // 0b10000000
+#define FLAG_LENGTH_0 0                // 0b0000000000000000
+#define FLAG_LENGTH_1 1                // 0b0000000000000001
+#define FLAG_LENGTH_2 2                // 0b0000000000000010
+#define FLAG_LENGTH_3 3                // 0b0000000000000011
+#define FLAG_LENGTH_4 4                // 0b0000000000000100
+#define FLAG_LENGTH_MASK 7             // 0b0000000000000111
+#define FLAG_BOLD      8               // 0b0000000000001000
+#define FLAG_FAINT     16              // 0b0000000000010000
+#define FLAG_ITALIC    32              // 0b0000000000100000
+#define FLAG_UNDERLINE 64              // 0b0000000001000000
+#define FLAG_STRIKEOUT 128             // 0b0000000010000000
+#define FLAG_BRACKETED_PASTE_MODE 256  // 0b0000000100000000
 
 // Represents a single unicode codepoint along with styling information such as
 // color, if it's bold, italic, etc. Fits snuggly in 88-bits.
 struct termbuf_char {
-    uint8_t utf8_char[4];
-    uint8_t flags;
-    uint8_t fg_color_r;
-    uint8_t fg_color_g;
-    uint8_t fg_color_b;
-    uint8_t bg_color_r;
-    uint8_t bg_color_g;
-    uint8_t bg_color_b;
+    uint8_t  utf8_char[4];
+    uint16_t flags;
+    uint8_t  fg_color_r;
+    uint8_t  fg_color_g;
+    uint8_t  fg_color_b;
+    uint8_t  bg_color_r;
+    uint8_t  bg_color_g;
+    uint8_t  bg_color_b;
 };
 
 enum parser_state {
@@ -38,15 +39,32 @@ enum parser_state {
     P_STATE_CHOMP1 = 1,
     P_STATE_CHOMP2 = 2,
     P_STATE_CHOMP3 = 3,
+    // The parser encountered the C0/C1 control character "ESC".
+    P_STATE_ESC    = 4,
+    // The parser has now encountered "ESC[", meaning we started an escape
+    // sequence.
+    P_STATE_CSI    = 5,
+    P_STATE_CSI_PARAMS = 6,
 };
-#define NSTATES 4
+#define NSTATES 7
 
 union parser_data {
     struct utf8_chomping {
         uint8_t len;
         uint8_t utf8_char[4];
     } utf8_chomping;
+    struct ansi_csi_chomping {
+        uint8_t  initial_char;  // one of '\0' (no initial char) or '?'
+        uint8_t  current_param;
+        uint16_t params[3];
+    } ansi_csi_chomping;
 };
+
+/* ESC[?2004 ->
+       initial_char   = '?'
+       n_params       = 1
+       params         = { 2004, 0, 0 }
+*/
 
 struct termbuf {
     int nrows;
