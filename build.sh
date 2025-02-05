279c95b45354c689b7659bae29c6840d422510f8
Emma Bastås <emma.bastas@protonmail.com>
2025-02-05 12:10:39 +0100

    Add a production build

--- build.sh
+++ build.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env sh
 
 printf "Choose an option\n"
-printf "  * debug build: d\n"
+printf "  * debug build      : d\n"
+printf "  * production build : p\n"
 read ans
 printf "\n"
 
 [ "$ans" == "d" ] && gcc -Og -std=gnu99 -pedantic -I glad/include/ -lc -lm -lharfbuzz -lX11 -lGLX -lGL CuTest.c min-terminal.c ringbuf.c termbuf.c rendering.c util.c glad/src/gl.c glad/src/glx.c -o min-terminal && exit $?
 
-printf "Expected you to type d.\n"
+[ "$ans" == "p" ] && gcc -O3 -std=gnu99 -pedantic -I glad/include/ -lc -lm -lharfbuzz -lX11 -lGLX -lGL CuTest.c min-terminal.c ringbuf.c termbuf.c rendering.c util.c glad/src/gl.c glad/src/glx.c -o min-terminal && exit $?
+
+
+printf "Expected you to type d or p.\n"
 exit 1
 
