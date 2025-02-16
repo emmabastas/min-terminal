1c958d975fea9e5bd4ea7cb23a80c92387571468
Emma Bastås <emma.bastas@protonmail.com>
2025-02-16 20:48:24 +0100

    Add esctest testing

--- build.sh
+++ build.sh
@@ -6,6 +6,8 @@ printf "  * Production build                       : p\n"
 printf "  * Build and run unit tests               : u\n"
 printf "  * Build and run unit tests with memcheck : um\n"
 printf "  * Run memcheck on ./min-terminal         : memcheck\n"
+printf "  * Download esctest test suite            : get esctest\n"
+printf "  * Run esctest                            : esctest\n"
 read ans
 printf "\n"
 
@@ -32,6 +34,10 @@ cmd=""
 && valgrind $memcheck_common_flags --suppressions=./tests/unit-tests-supressions.txt ./unit-test"
 
 [ "$ans" == "memcheck" ] && cmd="valgrind $memcheck_common_flag ./min-terminal"
+
+[ "$ans" == "get esctest" ] && cmd="cd ./tests && git clone git@github.com:ThomasDickey/esctest2.git --branch master --single-branch && cd esctest2 && git checkout fb8be26032ce4d5b8e05b2302d0492296aceec70 && cd ../../"
+
+[ "$ans" == "esctest" ] && cmd="./min-terminal ./tests/esctest2/esctest/esctest.py --max-vt-level=1 --expected-terminal=xterm"
 
 [ "$cmd" == "" ] && printf "Invalid input.\n" && exit 1
 
